2972b7f36955afe14eac63d35ae5663717288802
kamsuri <kamsuri5@gmail.com>
2017-03-31 22:25:31 +0530

    Minor changes

--- grab.py
+++ grab.py
@@ -6,6 +6,8 @@ import dateutil.parser
 import moment
 import dateparser
 import pandas as pd
+import time 
+import datetime	
 
 def download_photo(img_url,filename):
 
@@ -20,21 +22,17 @@ def download_photo(img_url,filename):
 def dateparse(filename):
 	for (k,v) in Image.open(filename)._getexif().iteritems():
 		if("date" in TAGS.get(k).lower()):
-			date,time=v.split()
-			#m = moment.date(v)
-			#print m.format('YYYY-M-D H:M')
-			#p=dateparser.parse(date)
-			print date
-			d = pd.to_datetime(time)
-			print d
 			print '%s = %s' % (TAGS.get(k), v)
-			#upload_date=datetime.datetime.now();
-			#print upload_date
+			upload_date=datetime.datetime.now().date;
+			print upload_date
 			#upload(orig_date,orig_time,upload_date,upload_time)
 
 
 def main():
-	test_url = ''
+	test_url = 'http://enigmata.csinsit.org/levels/try.jpg'
+	xmlstring=urlopen(test_url).read()
+    xmldoc = minidom.parseString(xmlstring)
+    itemlist = xmldoc.getElementsByTagName('channel')
 	name, ext = (test_url.split('/')[-1].split('.'))
 	filename=str(name)+"."+str(ext)
 	download_photo(test_url,filename)
