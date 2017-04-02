0521931e1d40260d652fd875d9a4971c430d9f22
kamsuri <kamsuri5@gmail.com>
2017-04-03 00:54:34 +0530

    Corrected coding errors

--- grab.py
+++ grab.py
@@ -1,35 +1,59 @@
-import urllib
+from urllib import urlopen
 import os,sys
 from PIL import Image
 from PIL.ExifTags import TAGS
-import dateutil.parser
-#import moment
-#import dateparser
-#import pandas as pd
-#import time 
-#import datetime	
+from dateutil import parser
+import time
+
+def get_month(mn):
+    months=['January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'Novemner',
+    'December'
+    ]
+    return months[mn-1]
 
 def download_photo(img_url,filename):
-
-	try:
-		with open(filename,'wb') as f:
-			f.write(urllib.urlopen(img_url).read())
-		dateparse(filename)
-	except:
-		return False
-	return True
+    
+    try:
+        with open(filename,'wb') as f:
+            f.write(urlopen(img_url).read())
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            print "File not found error"
+    except Exception as e:
+        print e
 
 def dateparse(filename):
-	for (k,v) in Image.open(filename)._getexif().iteritems():
-		if("date" in TAGS.get(k).lower()):
-			print '%s = %s' % (TAGS.get(k), v)
-			
-
+    
+    for k, v in Image.open(filename)._getexif().iteritems():
+        if "date" in TAGS.get(k).lower():
+            #print '%s = %s' % (TAGS.get(k), v)
+            conv = time.strptime(v,"%Y:%m:%d %H:%M:%S")
+            ti = time.strftime("%H:%M", conv)
+            day  = time.strftime("%d", conv)
+            year = time.strftime("%Y", conv)
+            month = time.strftime("%m", conv)
+            print '%s %s %s %s' % (ti, day, get_month(int(month)), year)
+            
 def main():
-	test_url = 'http://enigmata.csinsit.org/levels/try.jpg'
-	name, ext = (test_url.split('/')[-1].split('.'))
-	filename=str(name)+"."+str(ext)
-	download_photo(test_url,filename)
-
+    
+    test_url = 'http://enigmata.csinsit.org/levels/try.jpg'
+    filename = test_url.split('/')[-1]
+    f = urlopen(test_url)
+    if f.code == 200:
+        download_photo(test_url,filename)
+        dateparse(filename)
+    else:
+        print "File does not exist at given URL"
+    
 if __name__ == '__main__':
-	main()
+    main()
--- test.py
+++ test.py
@@ -1,16 +1,41 @@
-import urllib
+from urllib import urlopen
 import os,sys
 from dateutil import parser
 from xml.dom.minidom import parse, parseString
 from urllib import urlopen
 
+def get_month(mn):
+    months=['January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'Novemner',
+    'December'
+    ]
+    return months[mn-1]
+
 def main():
-	test_url = 'http://cultureelerfgoed.adlibsoft.com/harvest/wwwopac.ashx?database=images&search=pointer%201009%20and%20BE=%22interieur%22&limit=10&xmltype=grouped'
-	xmlstring=urlopen(test_url).read()
-	xmldoc = parseString(xmlstring)
-	itemlist = xmldoc.getElementsByTagName('production.date.start')
-	for elem in itemlist:
-		print parser.parse(elem.firstChild.nodeValue)
+    test_url = 'http://cultureelerfgoed.adlibsoft.com/harvest/wwwopac.ashx?database=images&search=pointer%201009%20and%20BE=%22interieur%22&limit=10&xmltype=grouped'
+    link = urlopen(test_url)
+    if link.code == 200:
+        xmlstring=link.read()
+        xmldoc = parseString(xmlstring)
+        itemlist = xmldoc.getElementsByTagName('production.date.start')
+        for elem in itemlist:
+            dt = parser.parse(elem.firstChild.nodeValue)
+            ti = dt.strftime("%H:%M")
+            day  = dt.strftime("%d")
+            year = dt.strftime("%Y")
+            month = dt.strftime("%m")
+            print '%s %s %s %s' % (ti, day, get_month(int(month)), year)
+    else:
+        print "File does not exist at given URL"
 
 if __name__ == '__main__':
-	main()
+    main()
